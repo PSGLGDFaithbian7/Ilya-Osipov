35c3a619a71cea1cfed584ac6fca214463779cc1
Yuhao Xie <129975898+PSGLGDFaithbian7@users.noreply.github.com>
2025-08-23 18:02:01 +0800

    2

--- exec/set_library.tcl
+++ exec/set_library.tcl
@@ -9,11 +9,26 @@ puts $fileToWrite
 while {[gets $fileToRead line] >= 0} {
     
    if {[regexp {LibraryPath:/s(/S+)} $line match LibraryPath]} {   #选用/S+避免捕获空格
-      puts "  $LibraryFile"
+      puts $fileToWrite "set_app_var  search_path $LibraryPath"
+      puts "--------------Library PATH--------------"
+	  puts "$LibraryPath"
    };
 
    if {[regexp {LibraryFile:/s(/S+)} $line match LibraryFile]} {
-    
+      puts $fileToWrite "set_app_var target_library  /"*$LibraryFile/""
+      puts $fileToWrite "set_app_var link_library  /"* $LibraryFile/""
+      puts "--------------LIBRARY_NAME--------------"
+      puts "$LibraryFile"
    };
     
-}
+    if {[regexp {TopModule:/s(/S+)} $line match TopModule]} {
+      puts $fileToWrite "set TopModule $TopModule"
+      puts "--------------TOP_NAME--------------"
+      puts "$TopModule"
+     
+   };
+    
+
+};
+
+close $fileToWrite;
