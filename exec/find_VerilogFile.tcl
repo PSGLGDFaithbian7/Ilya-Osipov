805caa8a22ecc6bb8839ee0ef401a7e46fb4e405
Yuhao Xie <129975898+PSGLGDFaithbian7@users.noreply.github.com>
2025-08-23 17:44:22 +0800

    1

--- exec/find_VerilogFile.tcl
+++ exec/find_VerilogFile.tcl
@@ -1,44 +1,41 @@
 #!/usr/bin/env tclsh
 
-set outputfile "./list/rtl_design.tcl";
+set outputfile "./list/rtl_design.list";
 set fileToWrite [open $outputfile w];
 
 proc find_HDLfiles {path} {
-   set flist{};
+   set flist {};#定义列表时列表名后加上空格
 
    if {[file isdirectory $path]} {
     set path [file normalize $path];
-    proc recursivefind {current_dir} {
-        set HDL_filelist{};
-        set current_hdlfile [glob -nocomplain -types f -directory $current_dir -- *.v *.sv *.vhdl];
-
-        foreach fileToAdd current_hdlfile {
-          lappend HDL_filelist $fileToAdd;  
-        }
-        
-
-        set subdirectory [glob -nocomplain -types d -directory  $current_dir -- *];
-        foreach SubdirCurrent $subdirectory {
-         set HDLinCurrentSubdir  [recursivefind $SubdirCurrent];
-         set  HDL_filelist [$HDL_filelist  $current_hdlfile];
-        }
-       return  HDL_filelist;
-    };
+    set flist [recursivefind $path]
+    return $flist;#要接收，不可以直接用
            
    } else {
      error "这不是一个文件夹: $path";
    };
-    
+}    
 
-    set flist [find_HDLfiles $path]
-    return $flist;#要接收，不可以直接用
-}
+#嵌套函数最好定义在外层
+proc recursivefind {current_dir} {
+        set HDL_filelist {};#定义列表时列表名后加上空格
+        set current_hdlfile [glob -nocomplain -types f -directory $current_dir -- *.v *.sv *.vhdl *.vh *.svh *.vhd];
+
+        foreach fileToAdd $current_hdlfile {  #循环列表时列表名字不要忘记$
+          lappend HDL_filelist $fileToAdd;  
+        }
+        set subdirectory [glob -nocomplain -types d -directory  $current_dir -- *];
+        foreach SubdirCurrent $subdirectory {
+         set HDLinCurrentSubdir  [recursivefind $SubdirCurrent];
+         set  HDL_filelist [concat $HDL_filelist  $HDLinCurrentSubdir];
+        }
+       return  $HDL_filelist;
+    }
 
 
+    set hdllist [find_HDLfiles [pwd]];
+    foreach f $hdllist {
+      puts $fileToWrite $f
+    };
 
-
-set flist [find_HDLfiles [pwd]];
-puts "找到了[llength $flist]个Verilog/SystemVerilog/VHDL文件";
-foreach f [lsort -dictionary $flist] {
-    puts $f
-};
+close $fileToWrite;
--- exec/set_library.tcl
+++ exec/set_library.tcl
@@ -1,1 +1,19 @@
 #!/usr/bin/env tclsh
+
+set fileToRead [open ./setup/library.lst r];
+set fileToWrite [open ./work/script.tcl a];
+
+puts $fileToWrite 
+
+
+while {[gets $fileToRead line] >= 0} {
+    
+   if {[regexp {LibraryPath:/s(/S+)} $line match LibraryPath]} {   #选用/S+避免捕获空格
+      puts "  $LibraryFile"
+   };
+
+   if {[regexp {LibraryFile:/s(/S+)} $line match LibraryFile]} {
+    
+   };
+    
+}
